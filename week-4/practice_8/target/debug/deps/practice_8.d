5a84499710a259e423476816dd94362a646dfbc4
s-sule <samuel.sule@pau.edu.ng>
2025-10-22 20:16:33 +0100

    (Added week-4 practice_8)

---
+++ week-4/practice_8/target/debug/deps/practice_8.d
@@ -0,0 +1,5 @@
+C:\Users\Sule\Documents\PAU COS101\s.suleCOS101\week-4\practice_8\target\debug\deps\practice_8.d: src\main.rs
+
+C:\Users\Sule\Documents\PAU COS101\s.suleCOS101\week-4\practice_8\target\debug\deps\practice_8.exe: src\main.rs
+
+src\main.rs:
---
+++ week-4/practice_8/target/debug/practice_8.d
@@ -0,0 +1,1 @@
+C:\Users\Sule\Documents\PAU\ COS101\s.suleCOS101\week-4\practice_8\target\debug\practice_8.exe: C:\Users\Sule\Documents\PAU\ COS101\s.suleCOS101\week-4\practice_8\src\main.rs
