caf9292327e162acc2646b5268164b78fb6a0859
s-sule <samuel.sule@pau.edu.ng>
2025-10-22 13:19:38 +0100

    (Week-4 practice_1 commit)

--- practice_1/target/debug/deps/practice_1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Sule\Desktop\PAU COS101\s.suleCOS101\week-3\practice_1\target\debug\deps\practice_1.d: src\main.rs
-
-C:\Users\Sule\Desktop\PAU COS101\s.suleCOS101\week-3\practice_1\target\debug\deps\practice_1.exe: src\main.rs
-
-src\main.rs:
--- practice_1/target/debug/practice_1.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Sule\Desktop\PAU\ COS101\s.suleCOS101\week-3\practice_1\target\debug\practice_1.exe: C:\Users\Sule\Desktop\PAU\ COS101\s.suleCOS101\week-3\practice_1\src\main.rs
